4c501f2bc79e1ca851a884f8cba7ad8fac4226f5
Shivani Sorte <sssorte1428@gmail.com>
2025-04-18 13:13:46 +0530

    no signup, data should be preloaded in firebase

--- gravity-web/src/App.js
+++ gravity-web/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import SignupForm from "./components/SignupForm";
 import HrDashboard from "./pages/HrDashboard";
 import ManagerDashboard from "./pages/ManagerDashboard";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
         <Route path="/hr-dashboard" element={<HrDashboard />} />
         <Route path="/manager-dashboard" element={<ManagerDashboard />} />
         <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
